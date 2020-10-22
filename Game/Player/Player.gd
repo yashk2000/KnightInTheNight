47ffa648b8fa22ad35db1add79cb94ed08bb3b3b
Shambhavi Aggarwal <agg.shambhavi@gmail.com>
2020-10-22 15:15:08 +0530

    game: add player and bush (#2)
    * game: add player and bush
    
    - add player scene
    - add bush scene
    - add some animation to player
    
    * Create .gitignore
    
    * .gitignore fix

---
+++ Game/Player/Player.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+const ACCELERATION = 500
+const MAX_SPEED = 80
+const FRICTION = 500
+
+var velocity = Vector2.ZERO
+
+onready var animationPlayer = $AnimationPlayer
+
+
+	
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
+			animationPlayer.play("RunRight")
+		else:
+			animationPlayer.play("RunLeft")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		animationPlayer.play("IdleRight")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+	velocity = move_and_slide(velocity )
+		
+	
