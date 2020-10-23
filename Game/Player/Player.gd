58fa64ba109742d637a54618c34970d8b09150c2
Shambhavi Aggarwal <agg.shambhavi@gmail.com>
2020-10-23 13:45:14 +0530

    Add hit effect
    - When the player kill the enemy, there is a small blast like effect
    - Linked player's heath to the player
    - Partially implemented the enemy's ability to kill the player

--- Game/Player/Player.gd
+++ Game/Player/Player.gd
@@ -13,6 +13,7 @@ enum {
 var state = MOVE
 var velocity = Vector2.ZERO
 var roll_vector = Vector2.DOWN
+var stats = PlayerStats
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
@@ -20,6 +21,7 @@ onready var animationState = animationTree.get("parameters/playback")
 onready var swordHitbox = $HitboxPivot/SwordHitbox
 
 func _ready():
+	stats.connect("no_health", self, "queue_free")
 	animationTree.active = true
 	swordHitbox.knockback_vector = roll_vector
 	
@@ -78,3 +80,7 @@ func roll_animation_finished():
 func attack_animation_finished():
 	state = MOVE
 	
+
+
+func _on_Hurtbox_area_entered(area):
+	stats.health -= 1
